d12900b70d6173714d13c7553894bdcc176e2d2b
Max p <max.permalnaick@ensg.eu>
2020-01-31 01:40:54 +0100

    création de la fonction get_name

--- main.cpp
+++ main.cpp
@@ -7,3 +7,12 @@ int main()
 	cout << "Hello World!" << endl;
 	return 0;
 }
+
+
+std::string get_name()
+{
+	std::string name;
+	std::cout << "Enter your name" << std::endl;
+	std::cin >> name;
+	return name;
+}
