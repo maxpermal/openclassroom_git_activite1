ea623322409e183ebe4c0845523a485548e1df32
Max p <max.permalnaick@ensg.eu>
2020-01-31 01:44:10 +0100

    oubli \n dans la fonction get_name : le nom collé à la phrase

--- main.cpp
+++ main.cpp
@@ -2,17 +2,25 @@
 
 using namespace std;
 
+//protoype fct
+std::string get_name();
+
 int main()
 {
 	cout << "Hello World!" << endl;
+	get_name ();
 	return 0;
 }
 
 
+/**
+ * @brief get_name demande d'entrer son nom
+ * @return
+ */
 std::string get_name()
 {
 	std::string name;
-	std::cout << "Enter your name" << std::endl;
+	std::cout << "Enter your name\n" << std::endl;
 	std::cin >> name;
 	return name;
 }
